56f3416919d7e26a38b2e0aecef7a868eeda9341
Gavin Stark <gstark31897@gmail.com>
2018-10-30 23:03:42 -0500

    working on ui

--- scripts/scene_0.lua
+++ scripts/scene_0.lua
@@ -3,31 +3,14 @@ require("gameobject")
 require("physics")
 require("light")
 require("camera")
+require("ui")
 
 function setup(scene, object)
+    test = ui_element_create(scene, nil)
+    ui_element_size(test, 0.2, 0.1)
+    ui_element_allign(test, "left", 0)
+    ui_element_allign(test, "bottom", 0.1)
 
-    --for x = 0, 10 do
-    --    for y = 0, 10 do
-    --      for i = 1, 2 do
-    --        cube = scene_add_gameobject(scene)
-    --        scene_set_model(scene, cube, "scifi_cube.dae")
-    --        gameobject_scale(cube, 1)
-    --        gameobject_transform(cube, x*1, i, y*1)
-    --        physics_init_box(scene, cube, 10, 0.5, 0.5, 0.5)
-    --      end
-    --    end
-    --end
-    cube = scene_add_gameobject(scene)
-    scene_set_model(scene, cube, "cube.dae")
-    gameobject_scale(cube, 0.5, 0.5, 0.5)
-    gameobject_transform(cube, 0, 10, 0)
-    physics_init_box(scene, cube, 1, 0.5, 0.5, 0.5)
-
-    --plane = scene_add_gameobject(scene)
-    --scene_set_model(scene, plane, "plane.dae")
-    --gameobject_transform(plane, 0, -0.1, 0)
-    --gameobject_scale(plane, 50)
-    --physics_init_box(scene, plane, 0, 50, 0.01, 50)
     example = scene_add_gameobject(scene)
     gameobject_transform(example, 0, 0, 0)
     scene_set_model(scene, example, "track_1.dae")
@@ -36,20 +19,9 @@ function setup(scene, object)
     scene_set_pbr(scene, example, "track_pbr.png")
     physics_init_mesh(scene, example, 0)
 
-    --balls = scene_add_gameobject(scene)
-    --gameobject_transform(balls, 0, 1, 0)
-    --scene_set_model(scene, balls, "pbr_balls.dae")
-    --scene_set_texture(scene, balls, "test.png")
-    --scene_set_pbr(scene, balls, "pbr_balls.png")
-
     ship = scene_add_gameobject(scene)
     gameobject_transform(ship, 0, 1.2, 0)
     scene_add_script(scene, ship, "ship.lua")
-
-    --player = scene_add_gameobject(scene)
-    --scene_set_model(scene, player, "capsule.dae")
-    --scene_add_script(scene, player, "player.lua")
-    --physics_set_position(player, 0, 1, 5)
 
     sun = scene_add_light(scene)
     scene_enable_shadow(scene, sun)
